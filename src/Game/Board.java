8dd7d6032b4a58ce0170a8f514913bb060c1e42f
jerry <2065572918@qq.com>
2025-05-02 17:36:53 +0800

    initial

---
+++ src/Game/Board.java
@@ -0,0 +1,78 @@
+package Game;
+
+
+public class Board {
+    String[][] board = new String[9][10];
+    Piece[] pieces = new Piece[32];
+
+    {
+        //red
+        pieces[0]=new Car(0,0,true);
+        pieces[1]=new Horse(1,0,true);
+        pieces[2]=new Minister(2,0,true);
+        pieces[3]=new Mandarin(3,0,true);
+        pieces[4]=new King(4,0,true);
+        pieces[5]=new Car(8,0,true);
+        pieces[6]=new Horse(7,0,true);
+        pieces[7]=new Minister(6,0,true);
+        pieces[8]=new Mandarin(5,0,true);
+        pieces[9]=new Solider(0,3,true);
+        pieces[10]=new Solider(2,3,true);
+        pieces[11]=new Solider(4,3,true);
+        pieces[12]=new Solider(6,3,true);
+        pieces[13]=new Solider(8,3,true);
+        pieces[14]=new Cannon(1,2,true);
+        pieces[15]=new Cannon(7,2,true);
+
+        //black
+        pieces[16]=new Car(0,9,false);
+        pieces[17]=new Horse(1,9,false);
+        pieces[18]=new Minister(2,9,false);
+        pieces[19]=new Mandarin(3,9,false);
+        pieces[20]=new King(4,9,false);
+        pieces[21]=new Car(8,9,false);
+        pieces[22]=new Horse(7,9,false);
+        pieces[23]=new Minister(6,9,false);
+        pieces[24]=new Mandarin(5,9,false);
+        pieces[25]=new Solider(0,6,false);
+        pieces[26]=new Solider(2,6,false);
+        pieces[27]=new Solider(4,6,false);
+        pieces[28]=new Solider(6,6,false);
+        pieces[29]=new Solider(8,6,false);
+        pieces[30]=new Cannon(1,7,false);
+        pieces[31]=new Cannon(7,7,false);
+    }
+
+    Board(){
+        for(int i=0;i<9;i++)
+            for(int j=0;j<10;j++)
+                board[i][j]="";
+
+        board[0][0]="车";board[1][0]="马";board[2][0]="相";
+        board[3][0]="士";board[4][0]="帅";board[5][0]="士";
+        board[6][0]="相";board[7][0]="马";board[8][0]="车";
+
+        board[0][9]="车";board[1][9]="马";board[2][9]="相";
+        board[3][9]="士";board[4][9]="帅";board[5][9]="士";
+        board[6][9]="相";board[7][9]="马";board[8][9]="车";
+
+        board[0][3]=board[2][3]=board[4][3]=board[6][3]=board[8][3]="兵";
+        board[0][6]=board[2][6]=board[4][6]=board[6][6]=board[8][6]="卒";
+
+        board[1][2]=board[7][2]=board[1][7]=board[7][7]="炮";
+
+    }
+
+    //将位于（ox，oy）的棋子移动到（nx,ny）
+    void move(int ox,int oy,int nx,int ny){
+        for (Piece piece : pieces) {
+            if(piece.x==ox && piece.y == oy){
+                if(piece.move(nx,ny,this.board)) {
+                    board[nx][ny] = board[ox][oy];
+                    board[ox][ox] = "";
+                }
+            }
+        }
+    }
+
+}
---
+++ src/Game/Main.java
@@ -0,0 +1,58 @@
+package Game;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public void main(String[] args) throws Exception {
+        // 要连接的服务端IP地址和端口
+        String host= "10.3.43.86";
+        int port = 55533;
+
+        // 与服务端建立连接
+        Socket socket = new Socket(host, port);
+
+        // 建立连接后获得输入输出流
+        OutputStreamWriter Writer = new OutputStreamWriter(socket.getOutputStream(),"UTF-8");
+        InputStreamReader Reader = new InputStreamReader(socket.getInputStream(), "UTF-8");
+
+        // 创建读取缓冲区
+        char[] buffer = new char[1024];
+        int length = Reader.read(buffer);
+        Writer.write("Successfully Connected");
+        Writer.flush();
+
+
+        //服务器分配红/黑方,初始时间
+        String response = new String(buffer, 0, length);
+
+
+
+
+
+        while(true){
+            length = Reader.read(buffer);
+            response = new String(buffer, 0, length);
+
+
+            // 模拟发送新消息
+            String message = "New message from client";
+            Writer.write(message);
+            Writer.flush(); // 刷新缓冲区，确保数据发送
+
+            if(response == "end") break;
+        }
+
+
+        Writer.close();
+        Reader.close();
+        socket.close();
+
+
+
+
+
+    }
+}
