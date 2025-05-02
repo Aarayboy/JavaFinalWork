dd33431e62a71dd430e86809e939680187adbd56
Jerry <2065572918@qq.com>
2025-05-02 19:12:47 +0800

    2025.5.2 19:12

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -52,9 +52,9 @@ public class Board {
         board[3][0]="士";board[4][0]="帅";board[5][0]="士";
         board[6][0]="相";board[7][0]="马";board[8][0]="车";
 
-        board[0][9]="车";board[1][9]="马";board[2][9]="相";
+        board[0][9]="车";board[1][9]="马";board[2][9]="象";
         board[3][9]="士";board[4][9]="将";board[5][9]="士";
-        board[6][9]="相";board[7][9]="马";board[8][9]="车";
+        board[6][9]="象";board[7][9]="马";board[8][9]="车";
 
         board[0][3]=board[2][3]=board[4][3]=board[6][3]=board[8][3]="兵";
         board[0][6]=board[2][6]=board[4][6]=board[6][6]=board[8][6]="卒";
